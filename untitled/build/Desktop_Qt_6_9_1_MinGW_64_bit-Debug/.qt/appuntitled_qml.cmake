ffa6147e4638c4e688217d746e75c5e19657900c
Maria Malcheva <87224276+mimsi420@users.noreply.github.com>
2025-07-23 11:45:27 +0100

    put the cool logo in the top left

---
+++ untitled/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/.qt/appuntitled_qml.cmake
@@ -0,0 +1,9 @@
+
+set(target "appuntitled")
+set(working_dir "D:/Documents/GitHub/imaginando-products")
+set(src_and_dest_list
+    "D:/Documents/GitHub/imaginando-products/Main.qml"
+    "D:/Documents/GitHub/imaginando-products/untitled/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/untitled/Main.qml"
+
+)
+set(timestamp_file "D:/Documents/GitHub/imaginando-products/untitled/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/.qt/appuntitled_qml.txt")
